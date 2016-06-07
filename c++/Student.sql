5df1a35bd214e1178f8c18656459f595822bfa05
zhanggen <zhanggen.jung@gmail.com>
2016-06-08 00:56:43 +0800

    add

---
+++ c++/Student.sql
@@ -0,0 +1,15 @@
+#CREATE DATABASE Student;
+
+#USE Student;
+
+CREATE TABLE Info
+(
+    id CHAR(8) NOT NULL,
+    name CHAR(10) NOT NULL,
+    CLanguage INT DEFAULT 0,
+    DataStruct INT DEFAULT 0,
+    DB INT DEFAULT 0,
+    TotalScore INT DEFAULT 0,
+    AverScore double DEFAULT 0,
+    PRIMARY KEY(id)
+);
